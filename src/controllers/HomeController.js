609ea1fc6b3998d80d962572d978450b719f8d2f
Erick-Rick <henrique.eryck@gmail.com>
2022-12-11 23:23:19 -0300

    create model aluno

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,5 +1,15 @@
+import Aluno from "../models/Aluno";
+
 class HomeController {
-  index(req, res) {
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Teste1',
+      sobrenome: 'Teste',
+      email: 'teste@teste.com',
+      idade: 30,
+      peso: 300,
+      altura: 1.9,
+    });
     res.json({
       tudoCerto: true,
     });
